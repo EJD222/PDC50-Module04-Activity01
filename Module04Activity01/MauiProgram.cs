bbec6dbbc56b8a0d7490b92ad785ee3faa2980d5
EJD222 <eljdiaz500@gmail.com>
2024-09-25 10:31:50 +0800

    Add Module04-Activity01 Files

---
+++ Module04Activity01/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace Module04Activity01
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+                .UseMauiCommunityToolkit()
+                // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("NunitoSans-Regular.ttf", "NunitoSansRegular");
+                    fonts.AddFont("NunitoSans-Semibold.ttf", "NunitoSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
